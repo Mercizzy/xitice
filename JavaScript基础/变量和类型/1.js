94d2821c2adb45905b9da246ed6543cd60375739
mercizzy <1978427972@qq.com>
2020-04-08 19:02:05 +0800

    js

--- JavaScript基础/变量和类型/1.js
+++ JavaScript基础/变量和类型/1.js
@@ -2,8 +2,9 @@
  * JavaScript规定了几种语言类型
  * 
  * 基本数据类型
-    Number String Undefined Null Boolean Symbol
-  引用数据类型
-    Object
+ *  Number String Undefined Null Boolean Symbol
+ * 
+ * 引用数据类型
+ *  Object
  */
 
---
+++ JavaScript基础/变量和类型/8.js
@@ -0,0 +1,52 @@
+/**
+ * 至少可以说出三种判断JavaScript数据类型的方式，
+ * 以及他们的优缺点，如何准确的判断数组类型
+ * 
+ * 1.typeof 优势在于判断基本数据类型，其中null为object
+ * 2.instanceof 检测原型
+ * 3.Object.prototype.toString.call() 最棒
+ */
+let a = 1,
+    b = "1",
+    c = undefined,
+    d = null,
+    e = true,
+    f = [],
+    g = {},
+    h = Symbol()
+
+console.log('----------typeof-----------');
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
+console.log(typeof h);
+console.log('----------typeof-----------');
+
+
+console.log('----------instanceof-----------');
+console.log(a instanceof Object);
+console.log(b instanceof String);
+console.log(c instanceof Object);
+console.log(d instanceof Object);
+console.log(e instanceof Boolean);
+console.log(f instanceof Object);
+console.log(g instanceof Object);
+console.log(h instanceof Symbol);
+console.log('----------instanceof-----------');
+
+console.log('----------Object.prototype.toString.call-----------');
+console.log(Object.prototype.toString.call(''));// [object String]
+console.log(Object.prototype.toString.call(1))  // [object Number]
+console.log(Object.prototype.toString.call(true)) // [object Boolean]
+console.log(Object.prototype.toString.call(undefined)) // [object Undefined]
+console.log(Object.prototype.toString.call(null)) ; // [object Null]
+console.log(Object.prototype.toString.call(new Function())) ; // [object Function]
+console.log(Object.prototype.toString.call(new Date())) ; // [object Date]
+console.log(Object.prototype.toString.call([])) ; // [object Array]
+console.log(Object.prototype.toString.call(new RegExp())) ; // [object RegExp]
+console.log(Object.prototype.toString.call(new Error())) ; // [object Error]
+console.log('----------Object.prototype.toString.call-----------');
